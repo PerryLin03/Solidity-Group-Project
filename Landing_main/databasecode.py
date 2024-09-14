a647095c7d558455b149bbabcf8fa4e65d85bb15
wileysdm <wileysdm@gmail.com>
2024-09-14 12:13:59 +0800

    Create databasecode.py

---
+++ Landing_main/databasecode.py
@@ -0,0 +1,48 @@
+#插入借款人信息
+r = request.form.get("q")
+    current_time =datetime.datetime.now()
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("insert into user values (?,?)",(r,current_time))
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('main.html',r=r)
+
+#插入存款人信息
+r = request.form.get("q")
+    current_time =datetime.datetime.now()
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("insert into user values (?,?)",(r,current_time))
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('main.html',r=r)
+
+#插入借款条约信息
+    depositor_address = request.form.get("depositor_address")
+    maxAmount = request.form.get("maxAmount")
+    minAmount = request.form.get("minAmount")
+    interestRate = request.form.get("interestRate")
+    collateralRate = request.form.get("collateralRate")
+    maxTime = request.form.get("maxTime")
+    act = 1
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("insert into user values (?,?,?,?,?,?,?)",(depositor_address,maxAmount,minAmount,interestRate,maxTime,collateralRate,act))
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('main.html')
+
+#插入借款记录信息
+r = request.form.get("q")
+    current_time =datetime.datetime.now()
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("insert into user values (?,?)",(r,current_time))
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('main.html',r=r)
